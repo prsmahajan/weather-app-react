78487a288d051a195aa82835ef757d6edd46c760
Paras Mahajan <prsmahajan@gmail.com>
2023-03-24 18:26:49 +0530

    padding removed

--- weather-project/src/WeatherData.js
+++ weather-project/src/WeatherData.js
@@ -5,24 +5,24 @@ import WeatherConvert from './WeatherConverter';
 
 export default function WeatherData(props){
     return(
-    <div className="WeatherData ps-3">
+    <div className="WeatherData">
            <h1>{props.weather.city}</h1>
             <ul>
                 <li><FormatDate date={props.weather.date} /></li>
                 <li className='desc'>{props.weather.description}</li>
             </ul>
-            <div className='row'>
+            <div className='row mb-5'>
                 <div className='col-6 mt-2'>
                     <WeatherIcon code={props.weather.icon} size={52} />
                     <WeatherConvert celsius={props.weather.temperature} />
                 </div>
-            <div className="col-6">
-                <ul>
-                    <li>Humidity: {props.weather.humidity}%</li>
-                    <li>Wind: {props.weather.wind}km/h</li>
-                </ul>
+                <div className="col-6">
+                    <ul>
+                        <li>Humidity: {props.weather.humidity}%</li>
+                        <li>Wind: {props.weather.wind}km/h</li>
+                    </ul>
+                 </div>
             </div>
-        </div>
     </div>
 )
 }
